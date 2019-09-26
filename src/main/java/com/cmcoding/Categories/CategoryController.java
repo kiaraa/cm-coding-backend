ea746f327d0e91ff57371f0bf0ed91807cb159d5
Kiara Atkins <atkinsk98@gmail.com>
2019-09-25 22:55:45 -0400

    Tested and expanded CategoryController

--- src/main/java/com/cmcoding/Categories/CategoryController.java
+++ src/main/java/com/cmcoding/Categories/CategoryController.java
@@ -2,7 +2,13 @@ package com.cmcoding.Categories;
 
 import com.cmcoding.Categories.Tip.Tip;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CategoryController {
+
+    TipCategory cat1 = new TipCategory(1, "Test category 1", Arrays.asList(new Tip(4, "This is altogether too many things in this line.")));
+    TipCategory cat2 = new TipCategory(2, "Test category 2", Arrays.asList(new Tip(3, "This is altogether too many things in this other line."), new Tip(5, "You absolute pedant.")));
 
     public Tip getSingleTipById(int catId, int tipId) {
         if (tipId == 2) {
@@ -10,6 +16,24 @@ public class CategoryController {
         }
         else {
             return new Tip(3, "test tip 2");
+        }
+    }
+
+    public TipCategory getCategoryById(int catId) {
+        if (catId == 1) {
+            return cat1;
+        }
+        else {
+            return cat2;
+        }
+    }
+
+    public List<Tip> getCategoryTips(int catId) {
+        if (catId == 1) {
+            return cat1.getTips();
+        }
+        else {
+            return cat2.getTips();
         }
     }
 
---
+++ src/main/java/com/cmcoding/Categories/TipCategory.java
@@ -0,0 +1,61 @@
+package com.cmcoding.Categories;
+
+import com.cmcoding.Categories.Tip.Tip;
+
+import java.util.List;
+import java.util.Objects;
+
+public class TipCategory {
+
+    private Integer id;
+    private String name;
+    private List<Tip> tips;
+
+    public TipCategory() {
+    }
+
+
+
+    public TipCategory(Integer id, String name, List<Tip> tips) {
+        this.id = id;
+        this.name = name;
+        this.tips = tips;
+    }
+
+    public List<Tip> getTips() {
+        return tips;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TipCategory category = (TipCategory) o;
+        return Objects.equals(id, category.id) &&
+                Objects.equals(name, category.name) &&
+                Objects.equals(tips, category.tips);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name, tips);
+    }
+
+    @Override
+    public String toString() {
+        return "TipCategory{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tips=" + tips +
+                '}';
+    }
+}
